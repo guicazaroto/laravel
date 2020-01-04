0ae8a1e823a91143b4d12e200dce8adaa791e97e
Guilherme Cazaroto <gui.cazaroto@gmail.com>
2020-01-04 01:51:38 -0300

    simplify routes

--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,31 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-# grupos de rotas em prefixos
-# dessa forma todas as rotas terão que derivar da rota '/dashboard/'
-Route::prefix('dashboard')->group(function () {
-
-    Route::namespace('Admin')->group(function () {
-        #chamando metodos em controllers
-        Route::get('/', 'AdminController@test');
-    });
-
-    Route::get('/settings', function () {
-        return 'Configurações do dashboar';
-    });
+Route::group([
+    'middleware' => [],
+    'prefix' => 'admin',
+    'namespace' => 'Admin',
+    'name' => 'admin.'
+], function () {
+   Route::get('/', 'AdminController@test')->name('home'); //this the 'admin.home'
 });
+
+
+// # grupos de rotas em prefixos
+// # dessa forma todas as rotas terão que derivar da rota '/dashboard/'
+// Route::prefix('dashboard')->group(function () {
+
+//     Route::namespace('Admin')->group(function () {
+//         #chamando metodos em controllers
+//         Route::get('/', 'AdminController@test');
+//     });
+
+//     Route::get('/settings', function () {
+//         return 'Configurações do dashboar';
+//     });
+// });
+
+
 
 // # Dessa forma garantimos que todas as rotas do grupo passarão pelo middleware auth
 // Route::middleware(['auth'])->group(function () {
@@ -53,31 +65,43 @@ Route::prefix('dashboard')->group(function () {
 //     return redirect()->route('new-name');
 // });
 
+
+
 // # redirecionamento de rotas
 // Route::redirect('/redirect1', '/redirect2');
 // Route::get('/redirect2', function () {
 //     return 'redirect 2';
 // });
 
+
+
 // # passando parametros via url
 // Route::get('/category/{flag}',function($flag) {
 //     return "Produtos da categoria {$flag}";
 // });
+
+
 
 // # tornando parametros opicionais
 // Route::get('/tag/{flag?}',function($tag = 'indefinida') {
 //     return "A flag é {$tag}";
 // });
 
+
+
 // # rota que aceita somente os métodos especificados
 // Route::match(['post', 'get'], '/match', function () {
 //     return 'match route';
 // });
 
+
+
 // # rota que aceita qualquer metodo
 // Route::any('/any', function () {
 //     return 'rota que aceita todos os métodos http';
 // });
+
+
 
 // Route::get('/contato', function () {
 //  return view('site.contato');
