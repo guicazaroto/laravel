3c26a2445c9b569ec890a47c3b3d90ef442ee7c9
Guilherme Cazaroto <gui.cazaroto@gmail.com>
2020-01-03 22:26:04 -0300

    add route groups middleware

--- routes/web.php
+++ routes/web.php
@@ -11,44 +11,61 @@
 |
 */
 
-Route::get('/route-name', function () {
-    return 'new name route';
-})->name('new-name');
-
-Route::get('/use-name',function () {
-    return redirect()->route('new-name');
-});
-
-# redirecionamento de rotas
-Route::redirect('/redirect1', '/redirect2');
-Route::get('/redirect2', function () {
-    return 'redirect 2';
-});
-
-# passando parametros via url
-Route::get('/category/{flag}',function($flag) {
-    return "Produtos da categoria {$flag}";
-});
-
-# tornando parametros opicionais
-Route::get('/tag/{flag?}',function($tag = 'indefinida') {
-    return "A flag é {$tag}";
-});
-
-# rota que aceita somente os métodos especificados
-Route::match(['post', 'get'], '/match', function () {
-    return 'match route';
-});
-
-# rota que aceita qualquer metodo
-Route::any('/any', function () {
-    return 'rota que aceita todos os métodos http';
-});
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contato', function () {
- return view('site.contato');
+
+# Dessa forma garantimos que todas as rotas do grupo passarão pelo middleware auth
+Route::middleware(['auth'])->group(function () {
+    Route::get('/hello', function () {
+        return 'Hello';
+    });
+
+    Route::get('/world', function () {
+        return 'world';
+    });
 });
+
+Route::get('/login', function () {
+    return 'Página de login';
+})->name('login');
+
+
+// Route::get('/route-name', function () {
+//     return 'new name route';
+// })->name('new-name');
+
+// Route::get('/use-name',function () {
+//     return redirect()->route('new-name');
+// });
+
+// # redirecionamento de rotas
+// Route::redirect('/redirect1', '/redirect2');
+// Route::get('/redirect2', function () {
+//     return 'redirect 2';
+// });
+
+// # passando parametros via url
+// Route::get('/category/{flag}',function($flag) {
+//     return "Produtos da categoria {$flag}";
+// });
+
+// # tornando parametros opicionais
+// Route::get('/tag/{flag?}',function($tag = 'indefinida') {
+//     return "A flag é {$tag}";
+// });
+
+// # rota que aceita somente os métodos especificados
+// Route::match(['post', 'get'], '/match', function () {
+//     return 'match route';
+// });
+
+// # rota que aceita qualquer metodo
+// Route::any('/any', function () {
+//     return 'rota que aceita todos os métodos http';
+// });
+
+// Route::get('/contato', function () {
+//  return view('site.contato');
+// });
