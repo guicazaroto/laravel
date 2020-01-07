8a5e3c4703c7d334f4e60b49081db6c69dcec3ab
Guilherme Cazaroto <gui.cazaroto@gmail.com>
2020-01-07 10:21:08 -0300

    controllers and middlewares

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,27 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
+# Definindo o resource obtemos rotas para todos os metodos Http's
+Route::resource('products', 'ProductController')->middleware('auth');
 
+Route::get('/login', function () {
+    return 'Página de login';
+})->name('login');
+
+
+/*
+Route::get('/products', 'ProductController@index')->name('products.index');
+Route::get('/products/{id}', 'ProductController@show')->name('products.show');
+Route::get('/products/create', 'ProductController@create')->name('products.create');
+Route::get('/products/{id}/edit', 'ProductController@edit')->name('products.edit');
+
+Route::post('/products', 'ProductController@store')->name('products.store');
+Route::put('/products/{id}', 'ProductController@update')->name('products.update');
+Route::delete('/products/{id}', 'ProductController@destroy')->name('products.destroy');
+*/
 
 # Agrupando diferentes tipos de rotas
 // Route::group(
@@ -56,10 +72,6 @@ Route::get('/', function () {
 //         return 'world';
 //     });
 // });
-
-// Route::get('/login', function () {
-//     return 'Página de login';
-// })->name('login');
 
 
 // Route::get('/route-name', function () {
