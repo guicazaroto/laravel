bc3e59cc4852b0dba4b89abfdd1ea5795433bdea
Guilherme Cazaroto <gui.cazaroto@gmail.com>
2020-01-08 09:08:01 -0300

    make an inline decision

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,15 +7,9 @@ use App\Models\Post;
 class PostController extends Controller
 {
     public function show ($slug) {
-        //return "This is my post {$slug}";
-
-        $post = Post::where('slug', $slug)->first();
-
-        if(!$post) abort(404);
-
         return view('post', 
             [
-                'post' => $post 
+                'post' => Post::where('slug', $slug)->firstOrFail()
             ]
         );
     }
