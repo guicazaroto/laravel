6797d8ebf6ef203bf05a37e69834e860e3ba686f
Guilherme Cazaroto <gui.cazaroto@gmail.com>
2020-01-17 23:39:07 -0300

    php artisan make:controller -r -m

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,57 +7,17 @@ use App\Article;
 
 class ArticleController extends Controller
 {
-    /**
-     * Render a list of resources
-     */
     public function Index()
     {
         $articles = Article::latest()->get();
         return view('articles.index', ['articles' => $articles]);
     }
     
-    /**
-     * Show a single resource
-    */
+
     public function show($id)
     {
         $article = Article::find($id);
         return view('articles.show', ['article' => $article]);
-    }
-
-    /**
-     * Shows a view to create a new resource
-     */
-    public function create () {
-
-    }
-
-    /**
-     * Persist a new resource
-     */
-    public function store () {
-
-    }
-
-    /**
-     * Show a view to edit an existing resource
-     */
-    public function edit () {
-
-    }
-
-    /**
-     * Persist an edited resource
-     */
-    public function update () {
-        
-    }
-
-    /**
-     * Delete a resoure
-     */
-    public function destroy () {
-
     }
 
 }
